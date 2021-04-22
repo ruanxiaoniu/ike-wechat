817d8e728268e2fd53d803ac874c96361e55ac36
ruanxiaoniu <2317735669@qq.com>
2021-04-22 17:19:21 +0800

    登录界面

---
+++ pages/login/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: RYL
+ * @Description: 
+ * @Date: 2021-04-16 12:02:32
+ * @LastEditTime: 2021-04-16 13:47:58
+ */
+Page({
+  data:{
+
+  },
+  Login: function(){
+    wx.navigateTo({
+      url: '/pages/dashboard/index',
+    })
+  }
+})
